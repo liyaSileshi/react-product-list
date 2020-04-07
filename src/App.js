c758c17d4b1f4406a181bd743e95accc1a098b2d
liyaSileshi <liyasileshi@gmail.com>
2020-04-07 16:10:01 -0700

    challenge 5 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {data, categories} from './data'
+import {data} from './data'
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {categories}
+          {/* {categories} */}
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
--- src/data.js
+++ src/data.js
@@ -9,23 +9,38 @@ const categories = data.map((item) => {
 //  Challenge 3 - Make the categories list a list of unique values.
 const categorie_set = new Set(categories) //    gives only unique categories
 const categories_unique = Array.from(categorie_set) //  make an array from the set
-// console.log(categories_unique)
+console.log(categories_unique)
 
 // Challenge 4 - Make an Object whose keys are the names of
 // categories and whose values are the number of times that
 // category appears in the data.
-const categoriesWithCount = data.reduce((obj, cat) => {
+const categoriesWithCount = data.reduce((obj, item) => {
     // check if cat exists as a key in an object
-    if (obj.hasOwnProperty(cat)){
+    if (obj[item.category] !== undefined){
         //if so add
-        obj[cat] += 1
+        obj[item.category] += 1
     }else{
         // set this key with a value of 1
-        obj[cat] = 1
+        obj[item.category] = 1
     }
     return obj
     }
     ,{})  //initial value of object is {}
-// console.log(Array.from(categoriesWithCount))
+console.log(categoriesWithCount)
+
+//  Challenge 5 - Use Reduce to make an array of objects 
+// that have a name and a count. This array will be similar 
+// to Challenge 4 but in a different format. For this list all 
+// of the names should be unique and each should only appear once!
+const categoriesWithCountName = categories_unique.reduce((arr, item) => {
+   arr.push({name: item, count: categoriesWithCount[item]})
+   return arr
+}
+,[])
+console.log(categoriesWithCountName)
+
+
+
+
 
 export {data, categories} //  export the native JS array
